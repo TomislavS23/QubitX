4da7bf179a2b315571b890734d6fae3818be1ab1
TomislavS23 <senjanin.tomislav@gmail.com>
2024-08-08 20:21:54 +0200

    modified SQL script and added inserts for courses, added AutoMapper support in WebApp, added some DTO classes to WebApp, refactored some of the API endpoints in WebAPI, implemented CourseTagController in WebAPI, implemented view for courses (for now without markdown support), courses are now visible on main page

--- WebApp/DataTransferObject/CourseDTO.cs
+++ WebApp/DataTransferObject/CourseDTO.cs
@@ -1,13 +1,14 @@
 namespace WebAPI.DataTransferObjects;
 
-public class CreateCourseDTO
+public class CourseDTO
 {
-    public string? Username { get; set; }
+    public int IdCourse { get; set; }
+
+    public int? IdUser { get; set; }
 
     public int? IdCourseType { get; set; }
 
     public string? CourseTitle { get; set; }
 
     public string? CourseContent { get; set; }
-    public IEnumerable<int> Tags { get; set; }
 }
